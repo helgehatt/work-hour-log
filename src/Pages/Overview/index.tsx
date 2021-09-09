31e661748694d15fde98955eae58efc369f47cb1
Helge Hatteland <helge.hatteland@sonat.no>
2021-09-09 20:59:34 +0200

    Removed donut charts

--- src/Pages/Overview/index.tsx
+++ src/Pages/Overview/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { useCalendar } from 'src/components/AppProviders/CalendarProvider';
 import styled from 'styled-components';
 import DurationsPerDate from 'src/components/Graphs/DurationsPerDate';
-import DurationsActiveMonth from 'src/components/Graphs/DurationsActiveMonth';
-import EntriesActiveMonth from 'src/components/Graphs/EntriesActiveMonth';
-import LongEntriesActiveMonth from 'src/components/Graphs/LongEntriesActiveMonth';
-import Grid from '@material-ui/core/Grid';
 
 (window as any).Apex.colors = ['#3f51b5', '#f50057'];
 
@@ -21,25 +16,9 @@ const Root = styled.div`
 `;
 
 const Overview: React.FC = () => {
-  const { showDuration } = useCalendar();
-
   return (
     <Root>
-      {showDuration ? (
-        <DurationsPerDate />
-      ) : (
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
-            <DurationsActiveMonth />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <EntriesActiveMonth />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <LongEntriesActiveMonth />
-          </Grid>
-        </Grid>
-      )}
+      <DurationsPerDate />
     </Root>
   );
 };
