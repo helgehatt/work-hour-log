bcce74f1edd94a8e23e2123eefec34882cb4d961
Helge Hatteland <helge.hatteland@sonat.no>
2021-09-29 21:46:22 +0200

    Added total and expected hours

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import moment from 'moment';
 import styled from 'styled-components';
 import AvTimerIcon from '@material-ui/icons/AvTimer';
 import BarChartIcon from '@material-ui/icons/BarChart';
+import ScheduleIcon from '@material-ui/icons/Schedule';
 import Divider from '@material-ui/core/Divider';
 import EventIcon from '@material-ui/icons/Event';
 import List from '@material-ui/core/List';
@@ -13,6 +15,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { useCalendar } from 'src/components/AppProviders/CalendarProvider';
 import { useHistory, useLocation } from 'react-router-dom';
+import CalendarHooks from 'src/components/AppHooks/CalendarHooks';
 
 interface IProps {}
 
@@ -21,7 +24,32 @@ const Root = styled.div``;
 const SideMenu: React.FC<IProps> = () => {
   const history = useHistory();
   const location = useLocation();
-  const { showDuration, setShowDuration } = useCalendar();
+  const { showDuration, setShowDuration, month } = useCalendar();
+  const entries = CalendarHooks.useEntries(month);
+
+  const totalHours = entries.reduce((acc, entry) => {
+    const start = moment.utc(entry.start);
+    const stop = moment.utc(entry.stop);
+    const break_ = (entry.break ?? '00:00')
+      .split(':')
+      .map(Number)
+      .map((v, i) => v * [60, 1][i]) // Convert hours to minutes
+      .reduce((x, y) => x + y, 0); // Sum list of minutes
+
+    const duration = stop.diff(start, 'minutes') - break_;
+
+    return acc + duration;
+  }, 0);
+
+  const expectedHours =
+    new Set(
+      entries
+        .map(entry => moment.utc(entry.start))
+        .filter(date => date.isoWeekday() < 6)
+        .map(date => date.format(moment.HTML5_FMT.DATE))
+    ).size *
+    7.5 *
+    60;
 
   const tab = location.pathname === '/overview' ? 'overview' : 'calendar';
 
@@ -63,6 +91,26 @@ const SideMenu: React.FC<IProps> = () => {
           </ListItemIcon>
           <ListItemText>Show durations</ListItemText>
           <Switch checked={showDuration} color='primary' />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <ScheduleIcon />
+          </ListItemIcon>
+          <ListItemText>Total hours</ListItemText>
+          <Typography>
+            {Math.floor(totalHours / 60)}:
+            {String(totalHours % 60).padStart(2, '0')}
+          </Typography>
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <ScheduleIcon />
+          </ListItemIcon>
+          <ListItemText>Expected hours</ListItemText>
+          <Typography>
+            {Math.floor(expectedHours / 60)}:
+            {String(expectedHours % 60).padStart(2, '0')}
+          </Typography>
         </ListItem>
       </List>
     </Root>
